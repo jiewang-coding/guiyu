3163bdcb07e102322f7c182e16cefc5d0c3178fb
wangjie <1435216151@qq.com>
2020-03-29 22:39:12 +0800

    注册、登录

---
+++ src/test/java/com/geek/guiyu/GuiyuApplicationTests.java
@@ -0,0 +1,26 @@
+package com.geek.guiyu;
+
+import com.geek.guiyu.service.util.ShortMessageUtil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class GuiyuApplicationTests {
+    @Autowired
+    ShortMessageUtil shortMessageUtil;
+    @Test
+    public void test() {
+        shortMessageUtil.sendShortMessage("15320263731");
+    }
+    @Test
+    public void testrandom(){
+        double random = Math.random() * 1000000;
+        System.out.println(String.valueOf((int)random));
+    }
+
+}
